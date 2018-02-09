ed9f830665a8087ae923c6cb5ce699e49fb1645d
Daniel Alexis <dalvarez.r@hotmail.com>
2018-02-09 02:27:36 -0600

    agregado el paquete de CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cors = require('cors');
 
 var app = express();
 
@@ -16,18 +17,20 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Configurar Cabeceras
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3700');
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-// allow preflight
-if (req.method === 'OPTIONS') {
-    res.send(200);
-} else {
-    next();
-}
-})
-;
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3700');
+// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+// res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// // allow preflight
+// if (req.method === 'OPTIONS') {
+//     res.send(200);
+// } else {
+//     next();
+// }
+// })
+// ;
 
 // rutas base
 app.use('/', express.static('client', {redirect: false}))
